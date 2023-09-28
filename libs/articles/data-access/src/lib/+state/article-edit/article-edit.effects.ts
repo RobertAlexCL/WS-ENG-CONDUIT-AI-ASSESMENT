d63d36b2e8eedb63f944d93e79fe4d2916b6875e
RobertAlexCL <rsstocast@gmail.com>
2023-09-27 19:17:35 -0600

    Assesment done

---
+++ libs/articles/data-access/src/lib/+state/article-edit/article-edit.effects.ts
@@ -0,0 +1,36 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { formsActions, ngrxFormsQuery } from '@realworld/core/forms';
+import { catchError, concatMap, map, of, tap } from 'rxjs';
+import { ArticlesService } from '../../services/articles.service';
+import { articleEditActions } from './article-edit.actions';
+
+export const publishArticle$ = createEffect(
+  (
+    actions$ = inject(Actions),
+    articlesService = inject(ArticlesService),
+    store = inject(Store),
+    router = inject(Router),
+  ) => {
+    return actions$.pipe(
+      ofType(articleEditActions.publishArticle),
+      concatLatestFrom(() => store.select(ngrxFormsQuery.selectData)),
+      concatMap(([_, data]) => {
+        let processedData = { ...data }; 
+        if (typeof processedData.tagList === 'string') {
+          processedData.tagList = processedData.tagList.split(',').map((tag: string) => tag.trim());
+        }
+      
+        return articlesService.publishArticle(processedData).pipe(
+          tap((result) => router.navigate(['article', result.article.slug])),
+          map(() => articleEditActions.publishArticleSuccess()),
+          catchError((result) => of(formsActions.setErrors({ errors: result.error.errors }))),
+        );
+      }),
+      
+    );
+  },
+  { functional: true },
+);
